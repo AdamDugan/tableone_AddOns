4788b01c62beb32c0a7a3928fd2a4e4c4da2b208
Adam J. Dugan <adam.dugan@uky.edu>
2022-04-26 12:23:00 -0400

    Update nsqip_subset_pufs.R
    Added the 2020 PUF to the function

--- nsqip_subset_pufs.R
+++ nsqip_subset_pufs.R
@@ -19,7 +19,7 @@
 
 ## The function
 nsqip_subset_pufs <- function(puf_folder_path = "Raw Files",
-                              puf_years = 2012:2019,
+                              puf_years = 2012:2020,
                               cpt_codes = NULL,
                               cpt_codes_primary_only = TRUE,
                               icd10_codes = NULL,
@@ -59,6 +59,7 @@ nsqip_subset_pufs <- function(puf_folder_path = "Raw Files",
   #   list(year = 2013, file = "Raw Files\\acs_nsqip_puf13.txt"),
   #   list(year = 2012, file = "Raw Files\\acs_nsqip_puf12.txt"))
   file_dictionary <- list(
+    list(year = 2020, file = paste0(puf_folder_path, "/acs_nsqip_puf20.txt"))
     list(year = 2019, file = paste0(puf_folder_path, "/acs_nsqip_puf19.txt")),
     list(year = 2018, file = paste0(puf_folder_path, "/acs_nsqip_puf18_v2.txt")),
     list(year = 2017, file = paste0(puf_folder_path, "/acs_nsqip_puf17.txt")),
